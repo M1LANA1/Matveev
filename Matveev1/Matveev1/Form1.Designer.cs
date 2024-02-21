fac567b3365d7fcf6a6b14dda78eca7d200bf910
M1LANA1 <160763276+M1LANA1@users.noreply.github.com>
2024-02-21 22:04:16 +0500

    0.2
    замена визуала

--- Matveev1/Matveev1/Form1.Designer.cs
+++ Matveev1/Matveev1/Form1.Designer.cs
@@ -34,28 +34,30 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(134, 157);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(60, 105);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(175, 50);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "НАЖМИ НА МЕНЯ";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(200, 92);
+            this.label1.Location = new System.Drawing.Point(113, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "ПРИВЕТ!";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(445, 291);
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(288, 178);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
--- Matveev1/Matveev1/Form1.cs
+++ Matveev1/Matveev1/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Matveev1
 {
     public partial class Form1 : Form
+
     {
+        Thread th;
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +22,14 @@ namespace Matveev1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Чао какао";
+            this.Close();
+            th = new Thread(open);
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+        public void open(object obj)
+        {
+            Application.Run(new Form2());
         }
     }
 }
